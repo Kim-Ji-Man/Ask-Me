19f2f6abb3148394f031ccf95ac37f7735a40939
jingeunlee4559 <wlsrms4559@gmail.com>
2024-10-16 09:07:19 +0900

    정보 수정 및 백엔드코드

---
+++ Backend/routes/MemberRouter.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/db');
+
+router.get('/', async (req, res) => {
+    const sql = 'SELECT * FROM Users';
+    try {
+        const results = await db.executeQuery(sql);
+        res.send(results);
+        console.log("가져와짐");
+    } catch (err) {
+        console.error('Error fetching cameras:', err);
+        return res.status(500).send('Error fetching cameras');
+    }
+    
+});
+
+router.post("/UpdateMember/:memberSeq", async (req, res) => {
+    let memberSeq = req.params.memberSeq; 
+    console.log("내정보 라우터:", memberSeq);
+  
+    const newEvent = req.body;
+  
+    let sql = `
+        UPDATE Users
+        SET username = ?,
+            role = ?,
+            gender = ?,
+            phone_number = ?,
+            account_status = ?
+        WHERE user_id = ?;
+    `; 
+  
+    const values = [
+      newEvent.username,
+      newEvent.role,
+      newEvent.gender,
+      newEvent.phone_number,
+      newEvent.account_status,
+      memberSeq 
+    ];
+    
+    try {
+        const results = await db.executeQuery(sql, values); 
+        res.send(results);
+        console.log("정보수정 완료");
+    } catch (err) {
+        console.error('Error fetching cameras:', err);
+        return res.status(500).send('Error fetching cameras');
+    }
+});
+
+router.delete('/Delete/:memberSeq', async (req, res) => {
+    let memberSeq = req.params.memberSeq;
+    console.log("딜리트 라우터:", memberSeq);
+    const sql = `DELETE FROM Users WHERE user_id=?`;
+
+    try {
+        const results = await db.executeQuery(sql,[memberSeq]); 
+        res.send(results);
+        console.log("회원삭제 완료");
+    } catch (err) {
+        console.error('Error fetching cameras:', err);
+        return res.status(500).send('Error fetching cameras');
+    }
+  });
+
+
+
+
+
+
+
+
+
+module.exports = router;
--- Frontend/src/components/UserModal.js
+++ Frontend/src/components/UserModal.js
@@ -1,86 +1,114 @@
-import React, { useEffect, useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
-import { Button } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import { Button } from "react-bootstrap";
 import Swal from "sweetalert2";
-
+import axios from"../axios"
 
 const UserModal = ({ show, handleClose, selectedMember }) => {
-  const [memberId, setMemberId] = useState('');
-  const [memberJic, setMemberJic] = useState('');
-  const [memberAt, setMemberAt] = useState('');
-  const [memberName, setMemberName] = useState('');
-  const [memberGender, setMemberGender] = useState('');
-  const [memberPhone, setMemberPhone] = useState('');
-  const [memberStatus, setMemberStatus] = useState('');
-  const [memberBusinessnumber, setMemberBusinessnumber] = useState('');
-  const [memberBusinessadress, setMemberBusinessadress] = useState('');
+  const [memberId, setMemberId] = useState("");
+  const [memberJic, setMemberJic] = useState("");
+  const [memberAt, setMemberAt] = useState("");
+  const [memberName, setMemberName] = useState("");
+  const [memberGender, setMemberGender] = useState("");
+  const [memberPhone, setMemberPhone] = useState("");
+  const [memberStatus, setMemberStatus] = useState("");
+  const [memberBusinessnumber, setMemberBusinessnumber] = useState("");
+  const [memberBusinessadress, setMemberBusinessadress] = useState("");
+  const [memberSeq ,setMemberSeq] = useState(0)
 
-
+  console.log(selectedMember, "sdfdsfsdfsdf");
 
   useEffect(() => {
     if (selectedMember) {
-      setMemberId(selectedMember.member_id);
-      setMemberJic(selectedMember.member_jic);
-      setMemberAt(selectedMember.member_at);
-      setMemberName(selectedMember.member_name);
-      setMemberGender(selectedMember.member_gender);
-      setMemberPhone(selectedMember.member_phone);
-      setMemberStatus(selectedMember.member_stauts);
-      setMemberBusinessnumber('SDFSDAFSAGDAS')
-      setMemberBusinessadress('광주 동구 충장로')
+      setMemberId(selectedMember.email);
+      setMemberJic(selectedMember.role === "admin" ? "admin" : "user");
+      setMemberAt(selectedMember.created_at.substring(0, 10));
+      setMemberName(selectedMember.username);
+      setMemberGender(selectedMember.gender);
+      setMemberPhone(selectedMember.phone_number);
+      setMemberStatus(selectedMember.account_status);
+      setMemberBusinessnumber(selectedMember.business_number === null ? " " : selectedMember.business_number);
+      setMemberBusinessadress("광주 동구 충장로");
+      setMemberSeq(selectedMember.user_id)
+      console.log(selectedMember.gender, "머로 나오니?");
+      console.log(selectedMember.user_id, "제대로??");
+
     }
   }, [selectedMember]);
 
+  function handelSave() {
+    handleClose(null);
 
-  function handelSave(){
-    handleClose(null)
-    
     Swal.fire({
-      title: '관리자비밀번호를 입력하세요',
-      input: 'password',
+      title: "관리자비밀번호를 입력하세요",
+      input: "password",
       inputAttributes: {
-        autocapitalize: 'off',
-        required: 'required',
-        minLength: 4
+        autocapitalize: "off",
+        required: "required",
+        minLength: 4,
       },
-      inputPlaceholder: '관리자 비밀번호 입력',
+      inputPlaceholder: "관리자 비밀번호 입력",
       showCancelButton: true,
-      confirmButtonText: '확인',
-      cancelButtonText: '취소'
+      confirmButtonText: "확인",
+      cancelButtonText: "취소",
     }).then((passwordResult) => {
       if (passwordResult.isConfirmed) {
         const inputPassword = passwordResult.value;
-
-
-        if (inputPassword === '0000') {
-          console.log(memberName,"바뀔까?");
+        
+        if (inputPassword === "0000") {
+          console.log(memberName, "바뀔까?");
           
-              Swal.fire({
-                icon: 'success',
-                text: '정보변경이 완료되었습니다.',
-                confirmButtonText: '확인'
-              });
+          const updatedInfo = {
+            email: memberId,
+            role: memberJic,
+            username :memberName,
+            gender:memberGender,
+            phone_number: memberPhone,
+            account_status:memberStatus,
+          };
+
+
+          axios.post(`/Member/UpdateMember/${memberSeq}`, updatedInfo)
+          .then(() => {
+            Swal.fire({
+              icon: 'success',
+              text: '정보변경이 완료되었습니다.',
+              confirmButtonText: '확인'
+            });
+          }).then(() => {
+            window.location.href = '/Member';
+          })
+          .catch((error) => {
+            console.error("Error updating:", error);
+            Swal.fire({
+              icon: 'error',
+              text: '정보변경 실패하였습니다.',
+              confirmButtonText: '확인'
+            });
+          });
         } else {
           Swal.fire({
-            icon: 'error',
-            text: '비밀번호가 일치하지 않습니다.',
-            confirmButtonText: '확인'
+            icon: "error",
+            text: "비밀번호가 일치하지 않습니다.",
+            confirmButtonText: "확인",
           });
         }
       }
     });
-  };
-  
+  }
 
   return (
     <Modal show={show} onHide={() => handleClose(null)}>
-      <Modal.Header closeButton style={{ backgroundColor: 'black', color: "white" }}>
+      <Modal.Header
+        closeButton
+        style={{ backgroundColor: "black", color: "white" }}
+      >
         <Modal.Title>회원수정</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Group className='my-3'>
+          <Form.Group className="my-3">
             <Form.Label>회원ID</Form.Label>
             <Form.Control
               type="text"
@@ -89,16 +117,20 @@ const UserModal = ({ show, handleClose, selectedMember }) => {
             />
           </Form.Group>
 
-          <Form.Group className='my-3'>
+          <Form.Group className="my-3">
             <Form.Label>직책</Form.Label>
             <Form.Control
-              type="text"
+              as="select"
               value={memberJic}
               onChange={(e) => setMemberJic(e.target.value)}
-            />
+            >
+              <option value="user">사용자</option>
+              <option value="admin">관리자</option>
+              
+            </Form.Control>
           </Form.Group>
 
-          <Form.Group className='my-3'>
+          <Form.Group className="my-3">
             <Form.Label>가입날짜</Form.Label>
             <Form.Control
               type="text"
@@ -108,7 +140,7 @@ const UserModal = ({ show, handleClose, selectedMember }) => {
             />
           </Form.Group>
 
-          <Form.Group className='my-3'>
+          <Form.Group className="my-3">
             <Form.Label>이름</Form.Label>
             <Form.Control
               type="text"
@@ -117,19 +149,19 @@ const UserModal = ({ show, handleClose, selectedMember }) => {
             />
           </Form.Group>
 
-          <Form.Group className='my-3'>
+          <Form.Group className="my-3">
             <Form.Label>성별</Form.Label>
             <Form.Control
               as="select"
               value={memberGender}
               onChange={(e) => setMemberGender(e.target.value)}
             >
-              <option value="남">남성</option>
-              <option value="여">여성</option>
+              <option value="man">남</option>
+              <option value="woman">여</option>
             </Form.Control>
           </Form.Group>
 
-          <Form.Group className='my-3'>
+          <Form.Group className="my-3">
             <Form.Label>전화번호</Form.Label>
             <Form.Control
               type="text"
@@ -138,26 +170,27 @@ const UserModal = ({ show, handleClose, selectedMember }) => {
             />
           </Form.Group>
 
-          <Form.Group className='my-3'>
+          <Form.Group className="my-3">
             <Form.Label>상태</Form.Label>
             <Form.Control
               as="select"
               value={memberStatus}
               onChange={(e) => setMemberStatus(e.target.value)}
             >
-            <option value="남">정상</option>
-            <option value="여">정지</option>
+              <option value="active">정상</option>
+              <option value="inactive">정지</option>
             </Form.Control>
           </Form.Group>
-          <Form.Group className='my-3'>
+          <Form.Group className="my-3">
             <Form.Label>사업자번호</Form.Label>
             <Form.Control
               type="text"
               value={memberBusinessnumber}
               onChange={(e) => setMemberBusinessnumber(e.target.value)}
+              disabled = {memberBusinessnumber == " "}
             />
           </Form.Group>
-          <Form.Group className='my-3'>
+          <Form.Group className="my-3">
             <Form.Label>주소</Form.Label>
             <Form.Control
               type="text"
